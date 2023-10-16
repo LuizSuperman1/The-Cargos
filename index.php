b9daa04353ddff258aed5c947f6be14e9bba1c2d
Davi Rocha <davipllays@gmail.com>
2023-10-16 17:39:21 -0300

    landing page

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./static/css/style-landing.css">
+    <title>Minha Landing Page</title>
+</head>
+
+<body>
+    <header>
+        <img src="/the_cargos/static/images/ascalcas.png" alt="as-calcas">
+        <a href="./templates/admlogin.php">Login ></a>
+    </header>
+    <div class="container">
+        <div class="section">
+            <h2>Quem Somos</h2>
+            <p>A The Cargos é uma loja especializada em calças e bermudas cargo em alto crescimento. Com o intuito de
+                melhorar o atendimento aos clientes, estamos desenvolvendo um sistema para gerência de estoque, assim
+                acelerando o atendimento e facilitando nossos serviços</p>
+        </div>
+        <div class="section">
+            <h2>Sobre Nossos Produtos</h2>
+            <p>São as calças e bermudas cargo com qualidade garantida. Entregamos para todo Brasil via Correios e para
+                toda região metropolitana de São Paulo via motoboy.</p>
+        </div>
+        <div class="section">
+            <h2>Formas de Contato</h2>
+            <div class="contact-info">
+                <p>Email: thecargos.anywhere@gmail.com</p>
+                <p>Telefone: +55 (11) 93208-2706</p>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Alguns de nossos Produtos</h2>
+            <div class="product-cards">
+                <div class="card">
+                    <div class="card-header">
+                        <img class="card-img" src="./static/images/calca-tactel-preta-ziper.jpg">
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-nome">Calça tactel preta com zíper</h3>
+                            <h2 class="card-preco">R$139,99</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <img class="card-img" src="./static/images/calca-tactel-cinza.jpeg">
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-nome">Calça tactel cinza</h3>
+                        <h2 class="card-preco">R$139,99</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <img class="card-img" src="./static/images/calca-parachute-verde.jpg">
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-nome">Calça parachute verde</h3>
+                        <h2 class="card-preco">R$139,99</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
