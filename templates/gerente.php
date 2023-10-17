b92428e75c8d48b68c090f462cd80c9e117f9576
Davi Rocha <davipllays@gmail.com>
2023-10-17 16:50:45 -0300

    barra branca da pagina inicial eliminada

--- templates/gerente.php
+++ templates/gerente.php
@@ -12,9 +12,7 @@ include("../static/php/protect.php");
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../static/css/style-gerente.css">
-    <link rel="stylesheet" href="../static/css/style-estoque-antigo.css">
-    <link rel="stylesheet" href="../static/css/style-estoque.css">
-    <link rel="stylesheet" href="../static/css/style-sidebar.css">
+    <link rel="stylesheet" href="../static/css/style-sidebar-gerente.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
